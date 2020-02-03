256fd4591052050faae1006b6b7a6f8abac7115b
squidtempura <d17129502@mytudublin.ie>
2020-02-03 19:16:15 +0000

    week2 code

--- java/src/ie/tudublin/HelloProcessing.java
+++ java/src/ie/tudublin/HelloProcessing.java
@@ -12,22 +12,24 @@ public class HelloProcessing extends PApplet
 	public void setup() {
 	}
 
-	/*
+	
 	public void draw() {
-		background(0, 0, 255);
-		stroke(255, 0, 0);
+		background(255, 0, 0);
+		noStroke();
 		fill(255, 255, 0);
-		line(10, 10, 100, 100);		// x1, y1, x2, y2
-		point(50, 50); // x, y
-		ellipse(100, 200, 50, 150); // cx, cy, w, h
-		fill(0, 255, 0);
-		rect(50, 10, 100, 200); // tlx, tly, w, h
-		triangle(10, 10, 100, 90, 200, 200); // vertices	
+		ellipse(250,300,400,400);
+		fill(0,255,255);
+		triangle(250,50,50,450,460,450);
+		fill(190, 190, 190);
+		ellipse(253,260,205,100);
+		fill(0,0,0);
+		ellipse(253,260,70,70);
+		
 	}	
-	*/
+	
 
 	int x = 0; // 4 bytes
-	
+	/*
 	public void draw()
 	{	
 		
@@ -37,4 +39,5 @@ public class HelloProcessing extends PApplet
 		ellipse(x, mouseY, 50, 50);		
 		x ++;
 	}
+	*/
 }
